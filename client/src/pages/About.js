735958b5e06eb8be76e528ce9afd5a452568cf44
Дмитрий Чванов <pocset2@gmail.com>
2024-02-25 16:40:00 +0300

    init

---
+++ client/src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './about.module.css'
+
+const About = () => {
+
+    return(
+        <div className={styles.main}>
+            <h1>МЫ самые крутые продавцы</h1>
+            <h2>продаем все и вся</h2>
+
+            <div className={styles.bike}>
+                <h1>прикупи огромный байк</h1>
+            </div>
+
+            <h3>Выполнили Чванов, Быков, Ендолов</h3>
+        </div>
+    ) 
+
+}
+
+export default About
